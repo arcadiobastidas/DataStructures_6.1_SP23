7db86a3937b9093cc6808f374266884d18d50b15
arcadiobastidas <abastidas31@gmail.com>
2023-04-05 06:46:02 -0400

    defining Integerset

--- DataStructures_6.1_SP23/DataStructures_6._1_SP23.cpp
+++ DataStructures_6.1_SP23/DataStructures_6._1_SP23.cpp
@@ -9,128 +9,62 @@ int main()
 {
     IntegerSet set1;
     IntegerSet set2;
-
-    //Implement an options Menu in the main program with the following operations:
-    //1. Add an element to set1
-    //2. Add an element to set2
-    //3. Remove an element from set1
-    //4. Remove an element from set2
-    //5. Clear set1
-    //6. Clear set2
-    //7. Check if an element is in set1
-    //8. Check if an element is in set2
-    //9. Print set1
-    //10. Print set2
-    //11. Print the union of set1 and set2
-    //12. Print the intersection of set1 and set2
-    //13. Print the difference of set1 and set2
-    //14. Print the difference of set2 and set1
-    //15. Exit
-
+    IntegerSet result;
+    int firstSet[5] = {1, 2, 3, 4, 5};
+    int secondSet[5] = {3, 4, 5, 6, 7};
     int choice;
     bool exit = false;
+    
+    //add firstSet to set1
+    // for(int i = 0; i < 5; i++)
+    // {
+    //     set1.add(firstSet[i]);
+    // }
+    // //add secondSet to set2
+    // for(int i = 0; i < 5; i++)
+    // {
+    //     set2.add(secondSet[i]);
+    // }
+
+
+
+    //Implement an options Menu in the main program with the following operations:
+    // 1.Print the two sets
+    // 2.Union of two sets
+    // 3.The intersection of two sets.
+    // 4.Difference between two sets.
+    // 5.Symmetric difference between two sets.
+    // 6.Verify if two sets are equal.
+    // 7.Print the set.
+    // 8.Quit the program.
+
 
     do
     {
+        cin >> choice;
         switch (choice)
         {
-            //1. Add an element to set1
+            // 1.Print the two sets
             case 1:
-                int element;
-                cout << "Enter an element to add to set1: ";
-                cin >> element;
-                set1.add(element);
+               cin >> set1;
+               cout << set1;
                 break;
-            //2. Add an element to set2
+            // 2.Union of two sets
             case 2:
-                cout << "Enter an element to add to set2: ";
-                cin >> element;
-                set2.add(element);
+                result = set1.unionOfSets(set2);
+                cout << "Union of two sets: ";
+                result.printSet();
+                cout << endl;
                 break;
-            //3. Remove an element from set1
-            case 3:
-                cout << "Enter an element to remove from set1: ";
-                cin >> element;
-                set1.remove(element);
-                break;
-            //4. Remove an element from set2
-            case 4:
-                cout << "Enter an element to remove from set2: ";
-                cin >> element;
-                set2.remove(element);
-                break;
-            //5. Clear set1
-            case 5:
-                set1.clear();
-                break;
-            //6. Clear set2
-            case 6:
-                set2.clear();
-                break;
-            //7. Check if an element is in set1
-            case 7:
-                cout << "Enter an element to check if it is in set1: ";
-                cin >> element;
-                if (set1.contains(element))
-                {
-                    cout << element << " is in set1." << endl;
-                }
-                else
-                {
-                    cout << element << " is not in set1." << endl;
-                }
-                break;
-            //8. Check if an element is in set2
-            case 8:
-                cout << "Enter an element to check if it is in set2: ";
-                cin >> element;
-                if (set2.contains(element))
-                {
-                    cout << element << " is in set2." << endl;
-                }
-                else
-                {
-                    cout << element << " is not in set2." << endl;
-                }
-                break;
-            //9. Print set1
-            case 9:
-                cout << "Set1: ";
-                cout << set1;
-                break;
-            //10. Print set2
-            case 10:
-                cout << "Set2: ";
-                cout << set2;
-                break;
-            //11. Print the union of set1 and set2
-            case 11:
-                cout << "Union of set1 and set2: ";
-                cout << set1 + set2;
-                break;
-            //12. Print the intersection of set1 and set2
-            case 12:
-                cout << "Intersection of set1 and set2: ";
-                cout << set1 * set2;
-                break;
-            //13. Print the difference of set1 and set2
-            case 13:
-                cout << "Difference of set1 and set2: ";
-                cout << set1 - set2;
-                break;
-            //14. Print the difference of set2 and set1
-            case 14:
-                cout << "Difference of set2 and set1: ";
-                cout << set2 - set1;
-                break;
-            //15. Exit
+            
 
-
+            default:
+                cout << "Invalid choice" << endl;
 
             
         }
     }
-    while (true);
+    while (choice <3);
     
     
 
--- DataStructures_6.1_SP23/IntegerSet.h
+++ DataStructures_6.1_SP23/IntegerSet.h
@@ -3,11 +3,14 @@
 #include "ArrayList.cpp"
 #include <iostream>
 
-//create a class called IntegerSet
+using namespace std;
+
+
+
 class IntegerSet
 {
 //declare one attribute called boolList of ArrayList<bool> type
-private:
+    private:
     ArrayList<bool> boolList;
     //declare a constant called MIN_VALUE
     static const int MIN_VALUE = 0;
@@ -17,87 +20,166 @@ private:
      
 
 public:
-    //declare a constructor
-    IntegerSet();
-    //declare a constructor with one parameter
-    IntegerSet(const ArrayList<bool>& boolList);
-    //
-    //declare a copy constructor
-    IntegerSet(const IntegerSet& integerSet);
-    //declare a destructor
-    ~IntegerSet();
-    //declare a method called add
-    void add(int number);
-    //declare a method called remove
-    void remove(int number);
-    //declare a method called contains
-    bool contains(int number);
-    //declare a method called clear
-    void clear();
-    //declare a method called getSize
-    int getSize();
-    //declare a method called unionOfSets
-    IntegerSet unionOfSets(IntegerSet& integerSet);
-    //declare a method called intersectionOfSets
-    IntegerSet intersectionOfSets(IntegerSet& integerSet);
-    //declare a method called differenceOfSets
-    IntegerSet differenceOfSets(IntegerSet& integerSet);
-    //declare a method called isSubsetOf
-    bool isSubsetOf(IntegerSet& integerSet);
-    //declare a method called isEqualTo
-    bool isEqualTo(IntegerSet& integerSet);
-    //declare a method called printSet
-    void printSet();
-    //oveload the operator <<
+    //define a constructor
+IntegerSet()
+{
+}
 
-    friend ostream& operator<<(ostream& out, const IntegerSet& integerSet)
+//define a constructor with one parameter
+IntegerSet(ArrayList<bool> boolList)
+{
+    //set the attribute boolList to the parameter boolList
+    this->boolList=boolList;
+}
+
+//define a function called add
+void add(int element)
+{
+    //add the element to the boolList
+    boolList.insert(element, true);
+}
+
+//define a function called remove
+void remove(int element)
+{
+    //create a for loop that iterates through the boolList to find the element
+    for(int i=0; i<boolList.getLength(); i++)
     {
-        //loop through the array
-        for (int i = 0; i < integerSet.boolList.getLength(); i++) {
-            //if the value at the index is true
-            if (integerSet.boolList.getEntry(i)) {
-                //print the index
-                out << i << " ";
-            }
+        //if the element is found
+        if(boolList[i]==element)
+        {
+            //remove the element from the boolList
+            boolList.remove(i);
         }
-        //return the output stream
+    }
+}
+
+//define a function called clear
+void clear()
+{
+    boolList.clear();
+}
+
+//define a function called isEmpty
+bool isEmpty()
+{
+    if(boolList.isEmpty())
+    {
+        return true;
+    }
+    return false;
+}
+
+//define a function called contains
+bool contains(int element)
+{
+    //create a for loop to find the element using getEntry
+    for(int i=0; i<boolList.getLength(); i++)
+    {
+        //if the element is found
+        if(boolList[i]==element)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+//define a function called unionOfSets
+IntegerSet unionOfSets(IntegerSet& set2)
+{
+    //create a new IntegerSet called unionSet
+    IntegerSet unionSet;
+    //create a for loop to iterate through the boolList
+    for(int i=0; i<boolList.getLength(); i++)
+    {
+        //add the element to the unionSet
+        unionSet.add(boolList[i]);
+    }
+    //create a for loop to iterate through the boolList of set2
+    for(int i=0; i<set2.boolList.getLength(); i++)
+    {
+        //add the element to the unionSet
+        unionSet.add(set2.boolList[i]);
+    }
+    //return the unionSet
+    return unionSet;
+}
+
+//define a function called intersectionOfSets
+IntegerSet intersectionOfSets(IntegerSet& set2)
+{
+    //create a new IntegerSet called intersectionSet
+    IntegerSet intersectionSet;
+    //create a for loop to iterate through the boolList
+    for(int i=0; i<boolList.getLength(); i++)
+    {
+        //if the element is found in set2
+        if(set2.contains(boolList[i]))
+        {
+            //add the element to the intersectionSet
+            intersectionSet.add(boolList[i]);
+        }
+    }
+    //return the intersectionSet
+    return intersectionSet;
+    
+}
+
+//define a function called differenceOfSets
+IntegerSet differenceOfSets(IntegerSet& set2)
+{
+    //create a new IntegerSet called differenceSet
+    IntegerSet differenceSet;
+    //create a for loop to iterate through the boolList
+    for(int i=0; i<boolList.getLength(); i++)
+    {
+        //if the element is not found in set2
+        if(!set2.contains(boolList[i]))
+        {
+            //add the element to the differenceSet
+            differenceSet.add(boolList[i]);
+        }
+    }
+    //return the differenceSet
+    return differenceSet;
+}
+
+//define a function called printSet
+void printSet()
+{
+    //create a for loop to iterate through the boolList
+    for(int i=0; i<boolList.getLength(); i++)
+    {
+        //print the element
+        cout<<boolList.getEntry(i)<<" ";
+    }
+    cout<<endl;
+    
+}
+    //overload the operator<<
+    friend ostream& operator<<(ostream& out, const IntegerSet& set)
+    {
+        //create a for loop to iterate through the boolList
+        for(int i=0; i<set.boolList.getLength(); i++)
+        {
+            //print the element
+            out<<set.boolList.getEntry(i)<<" ";
+        }
+        out<<endl;
         return out;
-        
     }
 
-    //overload the operator >>
-    friend istream& operator>>(istream& in, IntegerSet& integerSet);
-    //overload the operator =
-    IntegerSet& operator=(const IntegerSet& integerSet);
-    //overload the operator +
-    IntegerSet operator+(const IntegerSet& integerSet);
-    //overload the operator -
-    IntegerSet operator-(const IntegerSet& integerSet);
-    //overload the operator *
-    IntegerSet operator*(const IntegerSet& integerSet);
-    //overload the operator ==
-    bool operator==(const IntegerSet& integerSet);
-    //overload the operator !=
-    bool operator!=(const IntegerSet& integerSet);
-    //overload the operator <=
-    bool operator<=(const IntegerSet& integerSet);
-    //overload the operator >=
-    bool operator>=(const IntegerSet& integerSet);
-    //overload the operator <
-    bool operator<(const IntegerSet& integerSet);
-    //overload the operator >
-    bool operator>(const IntegerSet& integerSet);
-
-    //overload the operator []
-
-    
-    
-
-
-
-    
-}; // end IntegerSet
-
-
-
+    //overload the operator>>
+    friend istream& operator>>(istream& in, IntegerSet& set)
+    {
+        //declare an integer called element
+        int element;
+        //read the element
+        in>>element;
+        //add the element to the boolList
+        set.add(element);
+        return in;
+    }
+};
 #endif
