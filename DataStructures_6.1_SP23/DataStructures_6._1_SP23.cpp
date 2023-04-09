07ed69886ea0d8f876d163f25e8dd2d2a73dafec
arcadiobastidas <abastidas31@gmail.com>
2023-04-08 20:02:53 -0400

    try again

--- DataStructures_6.1_SP23/DataStructures_6._1_SP23.cpp
+++ DataStructures_6.1_SP23/DataStructures_6._1_SP23.cpp
@@ -11,13 +11,10 @@ int main()
     IntegerSet set1;
     IntegerSet set2;
     IntegerSet result;
-    int firstSet[5] = {1, 2, 3, 4, 5};
-    int secondSet[5] = {3, 4, 5, 6, 7};
-    for (int i = 0; i < 5; i++) {
-        set1.add(firstSet[i]);
-        set2.add(secondSet[i]);
-    }
+    int item = 1;
 
+    cin >> set1;
+    cout << set1 << endl;
 
     return 0;
     
